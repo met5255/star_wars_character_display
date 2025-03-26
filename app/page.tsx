a4855e794885e6cddd2d0d16a6005929cde0faf6
met5255 <mate19942@gmail.com>
2025-03-26 12:00:32 +0100

    Add Error Bounding, lazy load component loading the main page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,26 @@
 'use client'
 import { Box, Typography } from "@mui/material";
-import CharacterCards from "./components/CharacterCards";
-import ModalPanel from "./components/ModalPanel";
 import { CharacterProvider } from "./contexts/CharacterContext";
+import ErrorBoundary from "./components/ErrorBoundary";
+import { lazy, Suspense } from "react";
+
+
+const CharacterCards = lazy(() => import('./components/CharacterCards'));
+const ModalPanel = lazy(() => import('./components/ModalPanel'));
+const Loading = lazy(() => import('./components/Loading'));
 
 export default function Home() {
   return (
     <Box p={4} textAlign={"center"}>
-      <Typography variant="h3" fontFamily={"Star Wars, sans-serif"} >Star Wars Karakterek</Typography>
+      <Typography variant="h3" fontFamily={"Star Jedi"} >Star Wars Karakterek</Typography>
+      <ErrorBoundary>
       <CharacterProvider>
         <CharacterCards />
-        <ModalPanel />
+        <Suspense fallback={<Loading/>}>
+          <ModalPanel />
+        </Suspense>
       </CharacterProvider>
+      </ErrorBoundary>
     </Box>
   );
 }
